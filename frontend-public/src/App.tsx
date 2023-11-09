8a95f704e38c387e2b97748b26020a2299dcdb9e
alexjr101 <alex.j.ribes@gmail.com>
2023-11-09 13:11:47 -0600

    Done with responsive header

--- frontend-public/src/App.tsx
+++ frontend-public/src/App.tsx
@@ -4,7 +4,17 @@ function App() {
 
   return (
     <>
-        <h1>Desy's Sweet Treats</h1>
+        <header className='container w-2xl max-w-screen-xl mx-auto p-8 flex justify-between items-center'>
+            <h1 className='text-2xl'>DESY'S SWEET TREATS</h1>
+            <nav className='hidden sm:flex w-1/2 justify-between'>
+                <a>HOME</a>
+                <a>ABOUT</a>
+                <a>CONTACT</a>
+                <button>ORDER</button>
+            </nav>
+            <img className='sm:hidden w-8'
+                src='icons/hamburger-menu.svg'/>
+        </header>
     </>
   )
 }
